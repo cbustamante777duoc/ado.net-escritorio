d9c92c4751e8bd2fc42e6c5faeb299f7387ba5bc
cbustamante777duoc <cra.bustamante@alumnos.duoc.cl>
2021-04-11 20:41:01 -0400

    Primer commit

---
+++ MiPrimeraConexionConSqlServer/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿
+namespace MiPrimeraConexionConSqlServer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvClinica = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvClinica)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvClinica
+            // 
+            this.dgvClinica.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvClinica.Location = new System.Drawing.Point(177, 76);
+            this.dgvClinica.Name = "dgvClinica";
+            this.dgvClinica.Size = new System.Drawing.Size(489, 282);
+            this.dgvClinica.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dgvClinica);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvClinica)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvClinica;
+    }
+}
+
---
+++ MiPrimeraConexionConSqlServer/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace MiPrimeraConexionConSqlServer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //cnx es el nombre que se puso en app.Config
+            SqlConnection cn = new SqlConnection(ConfigurationManager.ConnectionStrings["cnx"].ConnectionString);
+            //consulta sql + sqlConnection
+            SqlCommand cmd = new SqlCommand("SELECT IIDCLINICA,NOMBRE,DIRECCION FROM Clinica WHERE BHABILITADO = 1 ",cn);
+            //ejecuta el sql comand
+            SqlDataAdapter sda = new SqlDataAdapter(cmd);
+            //instacia de la tabla
+            DataTable tabla = new DataTable();
+            //llenado de la tabla
+            sda.Fill(tabla);
+            //llenado gridView
+            dgvClinica.DataSource = tabla;
+
+
+        }
+    }
+}
