aee9fcc6635b24960dbdabe709451c47027019ce
cbustamante777duoc <cra.bustamante@alumnos.duoc.cl>
2021-04-12 22:33:52 -0400

    procedimiento almancendo filtrado

--- MiPrimeraConexionConSqlServer/Form1.Designer.cs
+++ MiPrimeraConexionConSqlServer/Form1.Designer.cs
@@ -30,6 +30,10 @@ namespace MiPrimeraConexionConSqlServer
         private void InitializeComponent()
         {
             this.dgvClinica = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtIdClinica = new System.Windows.Forms.TextBox();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.btnLimpiar = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dgvClinica)).BeginInit();
             this.SuspendLayout();
             // 
@@ -41,23 +45,68 @@ namespace MiPrimeraConexionConSqlServer
             this.dgvClinica.Size = new System.Drawing.Size(489, 282);
             this.dgvClinica.TabIndex = 0;
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(198, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "id clinica";
+            // 
+            // txtIdClinica
+            // 
+            this.txtIdClinica.Location = new System.Drawing.Point(300, 35);
+            this.txtIdClinica.Name = "txtIdClinica";
+            this.txtIdClinica.Size = new System.Drawing.Size(205, 20);
+            this.txtIdClinica.TabIndex = 2;
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(528, 30);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(75, 23);
+            this.btnBuscar.TabIndex = 3;
+            this.btnBuscar.Text = "buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Location = new System.Drawing.Point(618, 30);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(75, 23);
+            this.btnLimpiar.TabIndex = 4;
+            this.btnLimpiar.Text = "limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.txtIdClinica);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.dgvClinica);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dgvClinica)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.DataGridView dgvClinica;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtIdClinica;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.Button btnLimpiar;
     }
 }
 
--- MiPrimeraConexionConSqlServer/Form1.cs
+++ MiPrimeraConexionConSqlServer/Form1.cs
@@ -21,8 +21,7 @@ namespace MiPrimeraConexionConSqlServer
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            //metodo que recibe una consulta y una dataGridView
-            SQL.ListarConsultaSQL("SELECT IIDCLINICA,NOMBRE,DIRECCION FROM Clinica WHERE BHABILITADO = 1", dgvClinica);
+            Listar();
 
             ////cnx es el nombre que se puso en app.Config
             //SqlConnection cn = new SqlConnection(ConfigurationManager.ConnectionStrings["cnx"].ConnectionString);
@@ -37,6 +36,31 @@ namespace MiPrimeraConexionConSqlServer
             ////llenado gridView
             //dgvClinica.DataSource = tabla;
 
+        }
+
+        private void Listar() 
+        {
+            //metodo que recibe una consulta y una dataGridView
+            SQL.ListarConsultaSQL("SELECT IIDCLINICA,NOMBRE,DIRECCION FROM Clinica WHERE BHABILITADO = 1", dgvClinica);
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            string idClinica = txtIdClinica.Text;
+            SqlConnection cn = new SqlConnection(ConfigurationManager.ConnectionStrings["cnx"].ConnectionString);
+            SqlCommand cmd = new SqlCommand("BuscarClinicaPorID", cn);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@idClinica", idClinica);
+            DataTable table = new DataTable();
+            SqlDataAdapter dataAdapter = new SqlDataAdapter(cmd);
+            dataAdapter.Fill(table);
+            dgvClinica.DataSource = table;
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            Listar();
+            txtIdClinica.Text = "";
         }
     }
 }
