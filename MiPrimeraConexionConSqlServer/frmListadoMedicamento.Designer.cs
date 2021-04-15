5a1133b7ef39f3322a10c87f8c74aa0488cca214
cbustamante777duoc <cra.bustamante@alumnos.duoc.cl>
2021-04-14 22:21:38 -0400

    radio boton listo

---
+++ MiPrimeraConexionConSqlServer/frmListadoMedicamento.Designer.cs
@@ -0,0 +1,102 @@
+﻿
+namespace MiPrimeraConexionConSqlServer
+{
+    partial class frmListadoMedicamento
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvMedicamento = new System.Windows.Forms.DataGridView();
+            this.rbNombre = new System.Windows.Forms.RadioButton();
+            this.rbConcentracion = new System.Windows.Forms.RadioButton();
+            this.txtValor = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMedicamento)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvMedicamento
+            // 
+            this.dgvMedicamento.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvMedicamento.Location = new System.Drawing.Point(59, 115);
+            this.dgvMedicamento.Name = "dgvMedicamento";
+            this.dgvMedicamento.Size = new System.Drawing.Size(638, 253);
+            this.dgvMedicamento.TabIndex = 0;
+            // 
+            // rbNombre
+            // 
+            this.rbNombre.AutoSize = true;
+            this.rbNombre.Location = new System.Drawing.Point(72, 52);
+            this.rbNombre.Name = "rbNombre";
+            this.rbNombre.Size = new System.Drawing.Size(62, 17);
+            this.rbNombre.TabIndex = 1;
+            this.rbNombre.TabStop = true;
+            this.rbNombre.Text = "Nombre";
+            this.rbNombre.UseVisualStyleBackColor = true;
+            // 
+            // rbConcentracion
+            // 
+            this.rbConcentracion.AutoSize = true;
+            this.rbConcentracion.Location = new System.Drawing.Point(201, 52);
+            this.rbConcentracion.Name = "rbConcentracion";
+            this.rbConcentracion.Size = new System.Drawing.Size(94, 17);
+            this.rbConcentracion.TabIndex = 2;
+            this.rbConcentracion.TabStop = true;
+            this.rbConcentracion.Text = "Concentracion";
+            this.rbConcentracion.UseVisualStyleBackColor = true;
+            // 
+            // txtValor
+            // 
+            this.txtValor.Location = new System.Drawing.Point(350, 52);
+            this.txtValor.Name = "txtValor";
+            this.txtValor.Size = new System.Drawing.Size(197, 20);
+            this.txtValor.TabIndex = 3;
+            this.txtValor.TextChanged += new System.EventHandler(this.filtrar);
+            // 
+            // frmListadoMedicamento
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtValor);
+            this.Controls.Add(this.rbConcentracion);
+            this.Controls.Add(this.rbNombre);
+            this.Controls.Add(this.dgvMedicamento);
+            this.Name = "frmListadoMedicamento";
+            this.Text = "frmListadoMedicamento";
+            this.Load += new System.EventHandler(this.frmListadoMedicamento_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMedicamento)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvMedicamento;
+        private System.Windows.Forms.RadioButton rbNombre;
+        private System.Windows.Forms.RadioButton rbConcentracion;
+        private System.Windows.Forms.TextBox txtValor;
+    }
+}
---
+++ MiPrimeraConexionConSqlServer/frmListadoMedicamento.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MiPrimeraConexionConSqlServer
+{
+    public partial class frmListadoMedicamento : Form
+    {
+        public frmListadoMedicamento()
+        {
+            InitializeComponent();
+        }
+
+        private void frmListadoMedicamento_Load(object sender, EventArgs e)
+        {
+            rbNombre.Checked = true;
+            SQL.ListarProcedureSQL("uspListarMedicamentoPrograma2", dgvMedicamento);
+
+        }
+
+        private void filtrar(object sender, EventArgs e)
+        {
+            string valor = txtValor.Text;
+            if (rbNombre.Checked==true)
+            {
+                SQL.FiltradoProcemientoAlmacenado("uspConsultarMedicamentoPorNombre2", "@nombre", valor, dgvMedicamento);
+            }
+            else
+            {
+                SQL.FiltradoProcemientoAlmacenado("uspConsultarMedicamentoPorConcentracion2", "@concentracion", valor, dgvMedicamento);
+            }
+        }
+    }
+}
